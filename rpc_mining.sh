fb3c72344137acef9434cc6a4bae00512a4b0ab7
nguyengiapht <128656043+nguyengiapht@users.noreply.github.com>
2023-06-07 21:52:20 +0700

    Add files via upload

---
+++ rpc_mining.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
+while :; do
+    ./astrominer -w Yderoi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xe7c45s5tftfstqmet6gr -r dero.rabidmining.com:10300 -p rpc;
+    sleep 5;
+done
